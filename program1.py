f2274bd972dc942fe9069f785e109bb370136ff5
Aditya dubey <adi28dubey@gmail.com>
2024-10-07 14:32:04 +0530

    Done

--- program1.py
+++ program1.py
@@ -4,7 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        
+        for char in s:
+            if char in bracket_map.values(): 
+                stack.append(char)  
+            elif char in bracket_map.keys():  
+                if not stack:  
+                    return False  
+                top_element = stack.pop()  
+                if bracket_map[char] != top_element:  
+                    return False  
+        
+        return not stack
 
 
 
